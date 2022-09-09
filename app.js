a1d809d16bc8cdb5f817f81bbaa32be6d2de55c7
Manuel2u <edodoo011@st.ug.edu.gh>
2022-09-09 10:26:01 +0000

    Added Level 1 and Level 2

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+require("dotenv").config();
+const app = express();
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser : true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = process.env.SECRET;
+
+userSchema.plugin(encrypt, { secret : secret, encryptedFields: ['password'] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render('home');
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const email = req.body.username
+    const password = req.body.password
+
+    const user = new User({
+        email : email,
+        password : password
+    });
+
+    user.save(function(err){
+        if(err){
+            console.log(err)
+        }else{
+            res.render("secrets")
+        }
+    });
+
+});
+
+app.post("/login", function(req, res){
+    const email = req.body.username
+    const password = req.body.password
+
+    User.findOne({email : email}, function(err, foundResults){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundResults){
+                if(foundResults.password === password){
+                    res.render("secrets")
+                }
+            }
+        }
+
+    });
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server started on port 3000")
+});
