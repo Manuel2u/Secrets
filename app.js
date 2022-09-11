a5891bd2f792849abe39b62d16c6bc8cb9bb9128
Manuel2u <edodoo011@st.ug.edu.gh>
2022-09-11 18:36:22 +0000

    Added Level 3 security

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
-require("dotenv").config();
+const md5 = require('md5');
 const app = express();
 
 app.set('view engine','ejs');
@@ -18,10 +17,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret : secret, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -39,7 +34,7 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
     const email = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
 
     const user = new User({
         email : email,
@@ -58,7 +53,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const email = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
 
     User.findOne({email : email}, function(err, foundResults){
         if(err){
