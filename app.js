9c00a6e44f309be8fb99dad6aafeb0d1fdfea2c1
Manuel2u <edodoo011@st.ug.edu.gh>
2022-09-14 13:33:55 +0000

    Added oAuth 2.0 and google authentication

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 const session = require("express-session");
 const passportLocalMongoose = require("passport-local-mongoose");
 const passport = require("passport");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+require('dotenv').config();
 
 app.set('view engine','ejs');
 
@@ -28,20 +31,45 @@ mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser : true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
 
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
     res.render('home');
@@ -71,6 +99,16 @@ app.get("/logout", function(req,res){
     });
     res.redirect("/");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+  app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.post("/register", function(req, res){
     User.register({username : req.body.username}, req.body.password, function(err,result){
